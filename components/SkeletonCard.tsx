ae43c8f1284cd435cb9c4d5e1dddcd20c1102fa7
Lyonel Pierce <lyonel@live.com>
2023-07-23 16:40:20 -0400

    skeleton template

--- components/SkeletonCard.tsx
+++ components/SkeletonCard.tsx
@@ -3,9 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 const SkeletonCard = () => {
   return (
     <div className="flex flex-col items-center justify-center rounded-lg bg-white p-4 shadow">
-      <Skeleton className="h-48 w-full mb-4" />
-      <Skeleton className="h-6 w-1/2" />
-      <Skeleton className="h-4 w-1/4 mt-2" />
+      <Skeleton className="h-64 w-full" />
     </div>
   );
 };
