715a09821a89728eace0056fc8ceb488b7199b87
Lyonel Pierce <lyonel@live.com>
2023-07-26 10:03:07 -0400

    cron test

--- app/api/cron-job/route.ts
+++ app/api/cron-job/route.ts
@@ -1,19 +1,10 @@
 import { getUsers } from "@/lib/api-limit";
 import { getSubscribers } from "@/lib/subscription";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const users = await getUsers();
   const subscribers = await getSubscribers();
 
-  return new Response(
-    JSON.stringify({
-      users,
-      subscribers,
-    }),
-    {
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-      },
-    }
-  );
+  return new NextResponse("hello");
 }
