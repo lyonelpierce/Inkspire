1750b1efb852e15fbb9a1d64b20711fa8c219565
Lyonel Pierce <lyonel@live.com>
2023-07-26 10:19:22 -0400

    Test 2

--- app/api/cron-job/route.ts
+++ app/api/cron-job/route.ts
@@ -6,6 +6,8 @@ export async function GET() {
   const users = await getUsers();
   const subscribers = await getSubscribers();
 
+  console.log("Users:", users, "Subscribers:", subscribers);
+
   const subscriberIds = subscribers.map((subscriber) => subscriber);
 
   const usersNotInSubscribers = users.filter(
