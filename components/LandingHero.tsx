b43a7402099227e58bae1d54b9cacd83f782a8ef
Lyonel Pierce <lyonel@live.com>
2023-08-03 23:00:04 -0400

    Mobile Center

--- components/LandingHero.tsx
+++ components/LandingHero.tsx
@@ -13,7 +13,7 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold md:py-10 text-center space-y-5">
       <div className="flex flex-col md:flex-row items-center justify-between gap-10">
-        <div className="text-4xl sm:text-4xl md:text-6xl lg:text-7xl space-y-5 font-bold text-left p-10">
+        <div className="text-4xl sm:text-4xl md:text-6xl lg:text-7xl space-y-5 font-bold md:text-left p-10">
           <h1 className="text-5xl md:text-6xl">The Best AI Tool for</h1>
           <div className="h-28 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-5xl md:text-6xl text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500">
             <TypewriterComponent
@@ -28,7 +28,7 @@ export const LandingHero = () => {
             Generate tattoo designs 1000x times faster.
             <p>Unleash the power of AI.</p>
           </div>
-          <div className="flex flex-col w-fit gap-3">
+          <div className="flex flex-col md:w-fit gap-3">
             <Link
               href={isSignedIn ? "/generator" : "/sign-up"}
               className="flex justify-center"
